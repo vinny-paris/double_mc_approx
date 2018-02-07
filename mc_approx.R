24708737ff3c5c6a270fcdea18a2649c63d5adca
vinny-paris <vinny@iastate.edu>
2018-02-07 16:17:46 -0600

    some updates and hopefully faster

--- mc_approx.R
+++ mc_approx.R
@@ -1,32 +1,45 @@
 
-
+#Simple Comparison
+# 1/M * sum(indicator(y>x))
+#Above ran 2000 times
 mc_approx <- function(x){
-  holding <- NULL
-for(i in 1:2000){
-j <- rnorm(x)
-k <- rnorm(x)
-holding[i] <- sum(j<k)/x
-}
+holding <- sapply(1:2000, function(y){sum(rnorm(x)<rnorm(x))/x})
   return(holding)
 }
-mean(holding)
 
 
-mc_approx_double <- function(x){
-  holding <- NULL
-for(i in 1:2000){
-j <- rnorm(x)
-jj <- kronecker(j, rep(1, x))
-k <- rnorm(x)
-holding[i] <- sum(jj<k)/(x^2)
-}
+mc_approx_up <- function(x){
+  holding <- matrix(ncol=11, nrow = 2000)
+  holding <- sapply(1:2000, function(y){sum(rnorm(x)<rnorm(x))/x})
   return(holding)
 }
+
+#Double Comparison
+#1/M*sum_i(1/M*sum_j(y_i>x_j))
+#Above ran 2000 times
+#simplification of 1/M^2 * sum(expanded(y) > x)
+#x cycled
+
+
+mc_approx_double_up <- function(x){
+  holding <- matrix(ncol = 11, nrow = 2000)
+  fake_function <- function(y){sum(kronecker(rnorm(x), rep(1,x)) <rnorm(x))/x^2}
+  holding <- sapply(1:2000, fake_function)
+  return(holding)
+}
+
+cl <- makeCluster(2)
+mc_approx_double_par <- function(x){
+  cl <- makeCluster(2)
+  holding <- matrix(ncol = 11, nrow = 2000)
+  fake_function <- function(y){sum(kronecker(rnorm(x), rep(1,x)) <rnorm(x))/x^2}
+  holding <- parSapply(cl, 1:2000, fake_function)
+  return(holding)
+}
+
 
 
 mm <- c(100,200,300,400,500,600,700,800,900,1000,2000)
-small_loop <- 
+ka <- sapply(mm,  mc_approx_up)
 
-
-jack <- c(100,200)
-ka <- sapply(jack,  mc_approx)
+ki <- sapply(mm, mc_approx_double_old_up)
